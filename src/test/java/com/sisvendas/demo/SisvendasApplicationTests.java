7c9b7d6a8ad400462e4f61bb3d3682318df075b5
Josiel Morais <morais_josiel@hotmail.com>
2021-01-03 10:47:56 -0300

    Projeto criado

---
+++ src/test/java/com/sisvendas/demo/SisvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sisvendas.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SisvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
