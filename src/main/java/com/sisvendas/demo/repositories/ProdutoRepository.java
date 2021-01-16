f20dddc5aaf6fdfef0266d670d46775e8cd5d819
Josiel Morais <morais_josiel@hotmail.com>
2021-01-16 11:24:52 -0300

    Produto e associação muitos pra muitos

---
+++ src/main/java/com/sisvendas/demo/repositories/ProdutoRepository.java
@@ -0,0 +1,12 @@
+package com.sisvendas.demo.repositories;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.sisvendas.demo.domain.Produto;
+
+@Repository 
+public interface ProdutoRepository extends JpaRepository <Produto,Integer> {
+
+}
