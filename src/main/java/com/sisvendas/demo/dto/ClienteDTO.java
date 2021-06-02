09e4da4be14f06a54a185cd5e29c120449d64538
josielmorais <moraisjosiel1@gmail>
2021-06-01 22:13:16 -0300

    PUT, DELETE e GET para Cliente

--- src/main/java/com/sisvendas/demo/dto/ClienteDTO.java
+++ src/main/java/com/sisvendas/demo/dto/ClienteDTO.java
@@ -2,28 +2,36 @@ package com.sisvendas.demo.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 
 import org.hibernate.validator.constraints.Length;
 
-import com.sisvendas.demo.domain.Categoria;
+import com.sisvendas.demo.domain.Cliente;
 
-public class CategoriaDTO implements Serializable {
+public class ClienteDTO implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-
+	
 	private Integer id;
 	
 	@NotEmpty(message = "Preenchimento obrigatótrio")
-	@Length(min = 5 ,max = 80, message = "O tamanho deve ser entre 5 e 80 caracteres")
+	@Length(min = 5 ,max = 120, message = "O tamanho deve ser entre 5 e 120 caracteres")
 	private String nome;
-
-	public CategoriaDTO() {
+	
+	@NotEmpty(message = "Preenchimento obrigatótrio")
+	@Email(message = "Email inválido")
+	private String email;
+	
+	public ClienteDTO (){
+		
 	}
 	
-	public CategoriaDTO(Categoria obj) {
+	public ClienteDTO(Cliente obj) {
 		id = obj.getId();
 		nome = obj.getNome();
+		email = obj.getEmail();
+		
 	}
 	
 	
@@ -44,4 +52,14 @@ public class CategoriaDTO implements Serializable {
 		this.nome = nome;
 	}
 
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	
+	
 }
--- src/main/java/com/sisvendas/demo/services/ClienteService.java
+++ src/main/java/com/sisvendas/demo/services/ClienteService.java
@@ -1,10 +1,19 @@
 package com.sisvendas.demo.services;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.sisvendas.demo.domain.Cliente;
+import com.sisvendas.demo.dto.ClienteDTO;
 import com.sisvendas.demo.repositories.ClienteRepository;
+import com.sisvendas.demo.services.exceptions.DataIntegrityException;
 import com.sisvendas.demo.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -14,15 +23,49 @@ public class ClienteService {
 	private ClienteRepository repo;
 	
 	public Cliente find(Integer id) {
-		
-		Cliente obj = repo.getOne(id);
-		if (obj == null) {
-			throw new ObjectNotFoundException("Objeto não encontrado! Id: " + id
-			+ ", Tipo: " + Cliente.class.getName());
-			}
-		return obj;
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+
+	public Cliente insert(Cliente obj) {
+		return obj = repo.save(obj);
+	}
+
+	public Cliente update(Cliente obj) {
+		Cliente newObj = find(obj.getId());
+		updateData(newObj,obj);
+		return obj = repo.save(obj);
 	}
 	
+	private void updateData(Cliente newObj,Cliente obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir cliente porque há entidades relacionadas");
+
+		}
+	}
+
+	public List<Cliente> findAll() {
+		return repo.findAll();
+	}
+
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String direction,String orderBy) {
+
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
+	}
+	
+	public Cliente fromDTO(ClienteDTO objDTO) {
+		return new Cliente(objDTO.getId(),objDTO.getNome(),objDTO.getEmail(),null,null);
+	}
 	
 
 }
