142b4b802ae22b1bf159765c4b4bc2e04ab7dc6b
josielmorais <moraisjosiel1@gmail>
2021-07-08 13:00:12 -0300

    Documentação com Swagger - Parte 2

--- src/main/java/com/sisvendas/demo/resources/CategoriaResources.java
+++ src/main/java/com/sisvendas/demo/resources/CategoriaResources.java
@@ -22,6 +22,10 @@ import com.sisvendas.demo.domain.Categoria;
 import com.sisvendas.demo.dto.CategoriaDTO;
 import com.sisvendas.demo.services.CategoriaService;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResources {
@@ -29,7 +33,7 @@ public class CategoriaResources {
 	@Autowired
 	private CategoriaService service;
 	
-	
+	@ApiOperation(value="Busca por id")
 	@RequestMapping(value="/{id}",method=RequestMethod.GET)
 	public ResponseEntity<?> find(@PathVariable Integer id) {
 		
@@ -37,6 +41,7 @@ public class CategoriaResources {
 		return ResponseEntity.ok().body(obj);
 	}
 	
+	@ApiOperation(value="Retorna todas categorias")
 	@RequestMapping(method=RequestMethod.GET)
 	public ResponseEntity<List<CategoriaDTO>> findAll() {
 		
@@ -44,7 +49,7 @@ public class CategoriaResources {
 		List<CategoriaDTO> listDTO = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDTO);
 	}
-	
+	@ApiOperation(value="Retorna todas categorias com paginação")
 	@RequestMapping(value = "/page", method=RequestMethod.GET)
 	public ResponseEntity<Page<CategoriaDTO>> findPage(
 			@RequestParam(value = "page", defaultValue = "0") Integer page,
@@ -58,6 +63,10 @@ public class CategoriaResources {
 		return ResponseEntity.ok().body(listDTO);
 	}
 	
+	@ApiOperation(value="Remove categoria")
+	@ApiResponses(value = {
+			@ApiResponse(code = 400, message = "Não é possível excluir uma categoria que possui produtos"),
+			@ApiResponse(code = 404, message = "Código inexistente") })
 	@PreAuthorize("hasAnyRole('ADMIN')")
 	@RequestMapping(value="/{id}",method=RequestMethod.DELETE)
 	public ResponseEntity<Void> delete(@PathVariable Integer id) {
@@ -66,6 +75,7 @@ public class CategoriaResources {
 		return ResponseEntity.noContent().build();
 	}
 	
+	@ApiOperation(value="Atualiza categoria")
 	@PreAuthorize("hasAnyRole('ADMIN')")
 	@RequestMapping(value="/{id}",method=RequestMethod.PUT)
 	public ResponseEntity<Categoria> update(@Valid @RequestBody CategoriaDTO objDTO,@PathVariable Integer id) {
@@ -75,6 +85,7 @@ public class CategoriaResources {
 		return ResponseEntity.noContent().build();
 	}
 	
+	@ApiOperation(value="Inseri categoria")
 	@PreAuthorize("hasAnyRole('ADMIN')")
 	@RequestMapping(method=RequestMethod.POST)
 	public ResponseEntity<Void> insert (@Valid @RequestBody CategoriaDTO objDTO){
